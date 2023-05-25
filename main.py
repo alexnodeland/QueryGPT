7b04ab6fe7056e8c628848cf2bbead1cf0882c28
Alex Nodeland <nodeland.alex@gmail.com>
2023-05-25 14:36:38 -0400

    Implemeted JSON output fixing and parsing

--- main.py
+++ main.py
@@ -7,19 +7,31 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+from langchain.output_parsers import PydanticOutputParser, OutputFixingParser
+from pydantic import BaseModel, Field, validator
 from config import MODEL, TEMPERATURE, EXAMPLE_PATH, QUERY_LANGUAGE, QUERIES_PATH, CHUNK_SIZE
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 chat = ChatOpenAI(model=MODEL, temperature=TEMPERATURE)
 
+# Define your desired data structure.
+class OptimizedQueries(BaseModel):
+    query_optimized: str = Field(description="optimized database query")
+    note: str = Field(description="notes about optimization process")
+# Set up a parser + inject instructions into the prompt template.
+parser = PydanticOutputParser(pydantic_object=OptimizedQueries)
+format_instructions = parser.get_format_instructions()
+fixing_parser = OutputFixingParser.from_llm(parser=parser, llm=ChatOpenAI())
+
 # Build prompt
-template = "As an AI assistant, your task is to optimize database queries. Given a {query_language} query, " + \
-    "your job is to provide an optimized version of the query and any relevant notes about the optimization process. " + \
-    "Please structure your response as a JSON object, following this format: " + \
-    "{ 'query_optimized': 'Your optimized query here', 'note': 'Your notes about the optimization process here' }. " + \
-    "Ensure your response is complete and does not include any extraneous comments or text. " + \
+template = (
+    "As an AI assistant, your task is to optimize database queries. Given a {query_language} query, "
+    "your job is to provide an optimized version of the query and any relevant notes about the optimization process. "
+    "Ensure your response is complete and does not include any extraneous comments or text. "
     "Avoid shortening or truncating your answer. Share the output in its complete form."
+    "\n\n{format_instructions}"
+)
 
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
@@ -53,5 +65,10 @@ for query_chunk, file_chunk in zip(query_chunks, file_chunks):
     # Format queries as a line delimited list
     queries_formatted = "\n".join(query_chunk)
     chain = LLMChain(llm=chat, prompt=chat_prompt)
-output = chain.run(query_language=QUERY_LANGUAGE, example_input=example_input, example_output=example_output, queries=queries_formatted, format_instructions=format_instructions)
-print(output)
+    output = chain.run(query_language=QUERY_LANGUAGE, format_instructions=format_instructions, example_input=example_input, example_output=example_output, queries=queries_formatted)
+    try:
+        output_parsed = parser.parse(output)
+    except Exception as e:
+        print(e)
+        output_parsed = fixing_parser.parse(output)
+    print(output_parsed)
