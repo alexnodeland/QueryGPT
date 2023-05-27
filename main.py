d09fb0c622b471816b6b5563ee36013148f987e9
Alex Nodeland <nodeland.alex@gmail.com>
2023-05-27 09:54:40 -0400

    In progress implementation of csv parsed output

--- main.py
+++ main.py
@@ -8,21 +8,25 @@ from langchain.prompts.chat import (
     HumanMessagePromptTemplate,
 )
 from langchain.output_parsers import PydanticOutputParser, OutputFixingParser
-from pydantic import BaseModel, Field, validator
-from config import MODEL, TEMPERATURE, EXAMPLE_PATH, QUERY_LANGUAGE, QUERIES_PATH, CHUNK_SIZE
+from pydantic import BaseModel, Field
+from config import MODEL, TEMPERATURE, EXAMPLE_PATH, QUERY_LANGUAGE, QUERIES_PATH, CHUNK_SIZE, OUTPUT_PATH
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
+print("Starting script...")
 chat = ChatOpenAI(model=MODEL, temperature=TEMPERATURE)
-
+print("Loaded model successfully")
 # Define your desired data structure.
 class OptimizedQueries(BaseModel):
     query_optimized: str = Field(description="optimized database query")
     note: str = Field(description="notes about optimization process")
 # Set up a parser + inject instructions into the prompt template.
+print("Setting up parser...")
 parser = PydanticOutputParser(pydantic_object=OptimizedQueries)
 format_instructions = parser.get_format_instructions()
+print("Parser set up successfully, format instructions: \n", format_instructions)
 fixing_parser = OutputFixingParser.from_llm(parser=parser, llm=ChatOpenAI())
+print("Fixing parser set up successfully")
 
 # Build prompt
 template = (
@@ -32,7 +36,7 @@ template = (
     "Avoid shortening or truncating your answer. Share the output in its complete form."
     "\n\n{format_instructions}"
 )
-
+print("Building prompt...")
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
 example_human = SystemMessagePromptTemplate.from_template("{example_input}", additional_kwargs={"name": "example_user"})
@@ -41,34 +45,64 @@ example_ai = SystemMessagePromptTemplate.from_template("{example_output}", addit
 human_template = "{queries}"
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, example_human, example_ai, human_message_prompt])
-
+print("Prompt built successfully, prompt: \n", chat_prompt)
 # Load prompt example
+print("Loading prompt example...")
 example = pd.read_csv(EXAMPLE_PATH)
-example_query = str(example['query'][0])
-example_query_optimized = str(example['query_optimized'][0])
-example_note = str(example['note'][0])
+example_query_a = str(example['query'][0])
+example_query_optimized_a = str(example['query_optimized'][0])
+example_note_a = str(example['note'][0])
+example_query_b = str(example['query'][0])
+example_query_optimized_b = str(example['query_optimized'][0])
+example_note_b = str(example['note'][0])
 
 # Set prompt variables
-example_input = '"' + example_query + '"'
-example_output = '"' + example_query_optimized + '","' + example_note + '"'
+print("Setting prompt variables...")
+example_input = '"'+ example_query_a +'"\n\n' + \
+    '"'+ example_query_b +'"'
+example_output = '[{"query_optimized": "'+ example_query_optimized_a +'", "note": "'+ example_note_a +'"}, {"query_optimized": "'+ example_query_optimized_b +'", "note": "'+ example_note_b +'"}]'
+
 
 # Load queries from CSV
+print("Loading queries...")
 queries_df = pd.read_csv(QUERIES_PATH)
 queries = queries_df['query'].tolist()
 files = queries_df['file'].tolist()
 
+# Create an empty DataFrame to store the results
+output_df = pd.DataFrame(columns=["query", "file", "query_optimized", "note"])
+
 # Chunk queries into groups of CHUNK_SIZE
+print("Chunking queries...")
 query_chunks = [queries[i:i + CHUNK_SIZE] for i in range(0, len(queries), CHUNK_SIZE)]
 file_chunks = [files[i:i + CHUNK_SIZE] for i in range(0, len(files), CHUNK_SIZE)]
 
 for query_chunk, file_chunk in zip(query_chunks, file_chunks):
     # Format queries as a line delimited list
-    queries_formatted = "\n".join(query_chunk)
+    queries_formatted = "\n\n".join(query_chunk)
+    print("Formatted queries successfully: ", queries_formatted)
     chain = LLMChain(llm=chat, prompt=chat_prompt)
+    print("Created chain successfully, running queries...")
     output = chain.run(query_language=QUERY_LANGUAGE, format_instructions=format_instructions, example_input=example_input, example_output=example_output, queries=queries_formatted)
+    print("Ran queries successfully, parsing output...")
     try:
         output_parsed = parser.parse(output)
+        print("Parsed output successfully")
     except Exception as e:
         print(e)
         output_parsed = fixing_parser.parse(output)
-    print(output_parsed)
+        print("Parsed output with fixer")
+    print('\n', output_parsed, '\n')
+    # For each result in the output, append a new row to the DataFrame
+    for query, file, result in zip(query_chunk, file_chunk, output_parsed):
+        query_optimized, note = result
+        new_row = pd.DataFrame({
+            "query": [query],
+            "file": [file],
+            "query_optimized": [query_optimized],
+            "note": [note]
+        })
+        output_df = pd.concat([output_df, new_row], ignore_index=True)
+
+# Save the DataFrame to a CSV file
+output_df.to_csv(OUTPUT_PATH, index=False)
